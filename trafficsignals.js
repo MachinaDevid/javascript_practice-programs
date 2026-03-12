856c9369951f9aa36d4bbffdab7a5bc3d52fec12
devidmachina09@gmail.com <gangadharreddy296@gmail.com>
2026-03-12 11:51:32 +0530

    js programs

---
+++ trafficsignals.js
@@ -0,0 +1,18 @@
+let signal = "red";
+
+switch(signal){
+    case "red":
+        console.log("stop");
+        break;
+
+    case "green":
+        console.log("move");
+        break;
+
+    case "yellow":
+        console.log("wait")
+        break;
+
+    default:
+        console.log("move accourdingly")
+}
