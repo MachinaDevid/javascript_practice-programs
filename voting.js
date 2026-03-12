11434628c98d67d4ddb573c3d41b997f52e5b9b5
devidmachina09@gmail.com <gangadharreddy296@gmail.com>
2026-03-12 10:50:20 +0530

    js program

---
+++ voting.js
@@ -0,0 +1,8 @@
+console.log("check the voting eligibility of persons")
+let x=prompt("enter your age")
+if(x >=18 ){
+    console.log("eligible for vote")
+}
+else{
+    console.log("not eligible for voted")
+}
