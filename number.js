f765e7cf955565aac0b140ce4e730b0f7b29461a
devidmachina09@gmail.com <gangadharreddy296@gmail.com>
2026-03-12 11:01:28 +0530

    practice program

---
+++ number.js
@@ -0,0 +1,10 @@
+let x= 0;
+if(x < 0){
+    console.log("the given number is negative")
+}
+else if(x >  0){
+    console.log("the numbere is positive")
+}
+else{
+    console.log("the number is equal to zero")
+}
