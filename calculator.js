17a9ece141d114366038505f5c77706ed1a9d4aa
devidmachina09@gmail.com <gangadharreddy296@gmail.com>
2026-03-12 11:49:51 +0530

    js programs

---
+++ calculator.js
@@ -0,0 +1,17 @@
+let n = 1;
+switch(n){
+    case 1:
+        console.log("+");
+        break;
+    case 2:
+        console.log("-");
+        break;
+    case 3:
+        console.log("*");
+        break;
+    case 4:
+        console.log("/");
+        break;
+    default:
+        console.log("none");
+}
