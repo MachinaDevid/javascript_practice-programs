1527c966318a9939423ba100ac177a7e09a012f2
devidmachina09@gmail.com <gangadharreddy296@gmail.com>
2026-03-12 11:50:31 +0530

    js programs

---
+++ monthnames.js
@@ -0,0 +1,38 @@
+let x= 10;
+switch( x){
+    case 1:
+        console.log("january");
+        break;
+    case 2:
+        console.log("febr");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 5:
+        console.log("may");
+        break;
+    case 6:
+        console.log("june");
+        break;
+    case 7:
+        console.log("july");
+        break;
+    case 8:
+        console.log("august");
+        break;
+    case 9:
+        console.log("september");
+        break;
+    case 10:
+        console.log("october");
+        break;
+    case 11:
+        console.log("novem,ber");
+        break;
+    case 12:
+        console.log("december");
+        break;
+    default:
+        console.log("none")
+}
