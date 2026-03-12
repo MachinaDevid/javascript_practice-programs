988b5c367d0fc6b27b62e9e9a3925486609fc30d
devidmachina09@gmail.com <gangadharreddy296@gmail.com>
2026-03-12 11:51:00 +0530

    js programs

---
+++ rating.js
@@ -0,0 +1,14 @@
+let x =2 ;
+switch(x){
+    case 1:
+        console.log("good");
+        break;
+    case 2:
+        console.log("average");
+        break;
+    case 3:
+        console.log("poor");
+        break;
+    default:
+        console.log("none");
+}
