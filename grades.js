a10ee5af7b726a3bcce323ba0063c8840785fd2d
devidmachina09@gmail.com <gangadharreddy296@gmail.com>
2026-03-12 11:00:46 +0530

    practice program

---
+++ grades.js
@@ -0,0 +1,15 @@
+let x = 90;
+switch(true){
+    case (x>=90 && x<=100):
+        console.log("A");
+        break; 
+    case(x>=60 && x<= 89):
+        console.log("B");
+        break;
+    case(x>=35 && x<= 59):
+        console.log("C");
+        break;
+    default:
+        console.log("fail")
+    
+}
