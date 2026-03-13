a4652e04d6999db9485ac79beb560ebd02c78e87
MachinaDevid <devidmachina09@gmail.com>
2026-03-13 11:22:51 +0530

    Add files via upload

---
+++ checkinganumber.js
@@ -0,0 +1,7 @@
+console.log("checking a number in the array list");
+
+let arr = [1,2,3,4,5,6,7,8,9,90];
+
+let result = arr.filter(element => element === 3);
+
+console.log(result);
---
+++ doubler.js
@@ -0,0 +1,4 @@
+console.log("programs on map");
+let arr =[1,2,3,4,5,6,7,8]
+ let result=arr.map(element=>element*element);
+ console.log(result)
